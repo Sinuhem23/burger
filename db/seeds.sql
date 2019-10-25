7bcef4a8d2f723bc57af12d8039596d9b4c2e2dd
sinuhe Montero <sinuhem23@gmail.com>
2019-10-25 17:42:12 -0400

    burgers

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES("Pizza-V-Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES
+("BBQ-V-Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES("Avocado-V-Burger", true);
+
+
+
+
