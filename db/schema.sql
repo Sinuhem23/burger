f65b4e9d31a959027c7e05849cc6db10a41e2e06
sinuhe Montero <sinuhem23@gmail.com>
2019-10-27 23:48:41 -0400

    updates

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,7 @@
-
-DROP TABLE IF EXISTS burgers;
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
+  
 USE burgers_db;
 
+DROP TABLE IF EXISTS burgers;
 
 CREATE TABLE burgers(
 	id INTEGER auto_increment not null,
